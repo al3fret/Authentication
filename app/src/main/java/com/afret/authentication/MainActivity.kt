a647880f37ae1a6cfee7c1269e2ce59c91a812a3
@codersdc <developer@codersdc.com>
2023-01-07 17:47:25 +0400

    splash screen

--- app/src/main/java/com/afret/authentication/MainActivity.kt
+++ app/src/main/java/com/afret/authentication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.afret.authentication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -11,8 +12,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.afret.authentication.presentation.authentication.signin.SignInScreen
 import com.afret.authentication.presentation.authentication.signup.SignUpScreen
+import com.afret.authentication.presentation.splash.SplashViewModel
 import com.afret.authentication.route.AppNavigationGraph
 import com.afret.authentication.theme.AuthenticationTheme
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
@@ -23,11 +26,25 @@ import dagger.hilt.android.AndroidEntryPoint
 @ExperimentalAnimationApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+
+    private val splashViewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
         FirebaseApp.initializeApp(applicationContext)
+
+
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                splashViewModel.isLoading.value
+            }
+        }
+
+
 
         setContent {
             AuthenticationTheme {
---
+++ app/src/main/java/com/afret/authentication/presentation/splash/SplashViewModel.kt
@@ -0,0 +1,31 @@
+package com.afret.authentication.presentation.splash
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class SplashViewModel @Inject constructor() : ViewModel() {
+
+
+
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+
+    init {
+
+        viewModelScope.launch {
+
+            delay(1)
+            _isLoading.value = false
+        }
+    }
+
+}
